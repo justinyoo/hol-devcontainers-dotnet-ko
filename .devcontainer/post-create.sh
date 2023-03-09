a0cf07ee3e998dd8306c923d265834c8db7f603f
Justin Yoo <justin.yoojh@gmail.com>
2023-03-09 05:48:28 +0000

    Update devcontainer settings

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,7 @@
 npm i -g azure-functions-core-tools@4 --unsafe-perm true
 
 # Azure Bicep CLI
-# az bicep install
+az bicep install
 
 # Azure Static Web Apps CLI
 npm install -g @azure/static-web-apps-cli
